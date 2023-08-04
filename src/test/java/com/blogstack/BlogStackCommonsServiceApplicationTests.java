388a747384afd83d35afe32ee8aeca1387a69c33
Abhay Vishwakarma <abhay.vishwakarma@neosoftmail.com>
2023-08-04 15:10:01 +0530

    Commons Service configured and ready for development

---
+++ src/test/java/com/blogstack/BlogStackCommonsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogStackCommonsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
