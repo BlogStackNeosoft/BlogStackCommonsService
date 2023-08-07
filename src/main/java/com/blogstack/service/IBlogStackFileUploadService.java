6b5a24a03c7efb04805be25f2998f478828c4f89
Abhay Vishwakarma <abhay.vishwakarma@neosoftmail.com>
2023-08-07 20:04:05 +0530

    Upload File funtion partially Implemented, BucketName needs to resolve

---
+++ src/main/java/com/blogstack/service/IBlogStackFileUploadService.java
@@ -0,0 +1,14 @@
+package com.blogstack.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface IBlogStackFileUploadService {
+    ResponseEntity<File> convertMultiPartFileToFile(MultipartFile file) throws FileNotFoundException, IOException;
+
+    ResponseEntity<String> uploadFile(MultipartFile file) throws IOException;
+}
---
+++ src/main/java/com/blogstack/service/impl/BlogStackFileUploadService.java
@@ -0,0 +1,64 @@
+package com.blogstack.service.impl;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.blogstack.commons.BlogStackCommonConstants;
+import com.blogstack.service.IBlogStackFileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+@Service
+public class BlogStackFileUploadService implements IBlogStackFileUploadService {
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${application.bucket.profile}")
+    private String bucketNameProfile;
+
+    @Value("${application.bucket.blog}")
+    private String bucketNameBlog;
+
+    @Override
+    public ResponseEntity<File> convertMultiPartFileToFile(MultipartFile blogStackBlogImage) throws FileNotFoundException, IOException {
+        FileOutputStream fileOutputStream = null;
+        File convertedFile = null;
+        try {
+            convertedFile = new File(Objects.requireNonNull(blogStackBlogImage.getOriginalFilename()));
+            fileOutputStream = new FileOutputStream(convertedFile);
+            fileOutputStream.write(blogStackBlogImage.getBytes());
+        } finally {
+            assert fileOutputStream != null;
+            fileOutputStream.close();
+        }
+        return ResponseEntity.ok(convertedFile);
+    }
+
+    @Override
+    public ResponseEntity<String> uploadFile(MultipartFile blogStackProfilePhoto) throws IOException {
+        ResponseEntity<File> convertedFile = convertMultiPartFileToFile(blogStackProfilePhoto);
+        String fileName = System.currentTimeMillis() + BlogStackCommonConstants.INSTANCE.UNDERSCORE_STRING + blogStackProfilePhoto.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketNameProfile, fileName, convertedFile.getBody()));
+        URL url = s3Client.getUrl(bucketNameProfile, fileName);
+        return ResponseEntity.ok(url.toString());
+    }
+
+//    @Override
+//    public ResponseEntity<String> uploadFile(MultipartFile blogStackProfilePhoto) throws IOException {
+//        ResponseEntity<File> convertedFile = convertMultiPartFileToFile(blogStackProfilePhoto);
+//        String fileName = System.currentTimeMillis() + BlogStackCommonConstants.INSTANCE.UNDERSCORE_STRING + blogStackProfilePhoto.getOriginalFilename();
+//        s3Client.putObject(new PutObjectRequest(bucketName, fileName, convertedFile.getBody()));
+//        URL url = s3Client.getUrl(bucketName, fileName);
+//        return ResponseEntity.ok(url.toString());
+//    }
+}
